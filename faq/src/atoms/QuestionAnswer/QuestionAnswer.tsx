c90d93212191f7cd291de3375fb6fc2749c765a0
Nika Bostoganashvili <151997372+theBosto@users.noreply.github.com>
2024-11-27 01:43:47 +0400

    Update QuestionAnswer component

--- faq/src/atoms/QuestionAnswer/QuestionAnswer.tsx
+++ faq/src/atoms/QuestionAnswer/QuestionAnswer.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { QuestionAnswers } from '../../interfaces';
-import Up from '../../assets/Up.svg'
-import '../../App.css'
+import Down from '../../assets/Down.svg';
+import Up from '../../assets/Up.svg';
+import '../../App.css';
 
 const QuestionAnswer: React.FC<QuestionAnswers> = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAnswer = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="qa-item">
-      <div className="question questionAnswer">
+      <div
+        className={`question ${isOpen ? 'bold' : ''}`}
+        onClick={toggleAnswer}
+      >
         <p>{question}</p>
         <div>
-            <img src={Up} alt="UpIcon" />
+          <img src={isOpen ? Up : Down} alt="toggleIcon" />
         </div>
-        
       </div>
-      <div className="qa-answer">{answer}</div>
-      <div className='line'></div>
+      <div className="line"></div>
+
+      {isOpen && <div className="qa-answer">{answer}</div>}
     </div>
   );
 };
--- faq/src/moleculs/RightSide/RightSide.tsx
+++ faq/src/moleculs/RightSide/RightSide.tsx
@@ -1,18 +1,30 @@
-import Question from "../../atoms/Question/Question";
 import QuestionAnswer from "../../atoms/QuestionAnswer/QuestionAnswer";
 
 const RightSide = () => {
   return (
     <div>
       <h1>FAQ</h1>
-      <Question question="How many team members can I invite?" />
+      {/* <Question question="How many team members can I invite?" /> */}
+      <QuestionAnswer
+        question="How many team members can i invite?"
+        answer="As many as you want."
+      />
       <QuestionAnswer
         question="What is the maximum file upload size?"
         answer="No more than 2GB. All files in your account must fit your allotted storage space."
       />
-      <Question question="How do I reset my password?" />
-      <Question question="Can I cancel my subscription?" />
-      <Question question="Do you provide additional support?" />
+      <QuestionAnswer
+        question="How do i reset my password"
+        answer="You can reset your password in your profile."
+      />
+      <QuestionAnswer
+        question="Can I cancel my subscription?"
+        answer="Of course. Whether you want"
+      />
+      <QuestionAnswer
+        question="Do you provide additional support?"
+        answer="Of course. 24/7"
+      />
     </div>
   );
 };
